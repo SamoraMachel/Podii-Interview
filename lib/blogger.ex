f7ae9a202eb0dac172fba9ffb87d8232246b7323
Samora Machel <samora@me.laptop>
2023-04-10 17:52:10 +0300

    Created a function to make a get request to a specified url

--- lib/blogger.ex
+++ lib/blogger.ex
@@ -3,14 +3,32 @@ defmodule Blogger do
   Documentation for `Blogger`.
   """
 
+  @blog_url "https://medium.com/podiihq/quick-connect-to-your-amazon-ec2-linux-instance-through-the-command-line-6c682960ef91"
+
+
+  @doc """
+  Fetch Blog
+  Uses the HTTPoison Library to make a get request to a blog url (@blog_url)
+
+``# Example
+
+    iex> Blogger.fetch_blog()
+    %HttpResponse{...}
+  """
+  @spec fetch_blog() :: HTTPoison.Response
+  def fetch_blog do
+    HTTPoison.start()
+    HTTPoison.get!(@blog_url)
+  end
+
   @doc """
   Capitalize list
   Takes in a list input and capitalizes each and every item in the list
 
   # Example
 
-      iex> Blogger.capitalize_list(["cow", "goat", "sheep"])
-      ["COW", "GOAT", "SHEEP"]
+    iex> Blogger.capitalize_list(["cow", "goat", "sheep"])
+    ["COW", "GOAT", "SHEEP"]
   """
   def capitalize_list(list) do
     Enum.map(list, &String.upcase(&1))
