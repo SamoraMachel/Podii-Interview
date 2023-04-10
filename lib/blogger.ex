9a09a5f3a17d4604ee3c9c8d568eafe058e10298
Samora Machel <samora@me.laptop>
2023-04-10 15:21:46 +0300

    Added code for capitalizing a word and a list of words

--- lib/blogger.ex
+++ lib/blogger.ex
@@ -3,11 +3,6 @@ defmodule Blogger do
   Documentation for `Blogger`.
   """
 
-  
-  defp capitalize_word(word) do
-    String.upcase(word)
-  end
-
   @doc """
   Capitalize list
   Takes in a list input and capitalizes each and every item in the list
@@ -18,6 +13,6 @@ defmodule Blogger do
       ["COW", "GOAT", "SHEEP"]
   """
   def capitalize_list(list) do
-    Enum.map(list, &capitalize_word(&1))
+    Enum.map(list, &String.upcase(&1))
   end
 end
